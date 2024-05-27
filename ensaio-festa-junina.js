a84743124d044ab92c3271c3531d6bd7b122d234
Enzohds <112430095+Enzohds@users.noreply.github.com>
2024-05-27 17:28:16 -0300

    Update ensaio-festa-junina.js

--- ensaio-festa-junina.js
+++ ensaio-festa-junina.js
@@ -86,6 +86,7 @@ banheiro[8] = new Banheiro("Banheiro 8", "Corredor H", 3, "azulejos azuis", "pia
 
 
 
+
 class Pessoas {
     constructor (musicalidade, corpo, idade, peso, cor, coordenacao_motora, altura, ritmo, nome, ano_nascimento, mes_nascimento, dia_nascimento, nome_pai, nome_mae, rg, cpf, endereco, numero_endereco, bairro, cidade, estado, cep, whatsapp, facebook, instagram, twiter_x, linkedin, email, tiktok, youtube, snapchat, orkut, tinder, uber, app_99, profissao, nacionalidade, estado_civil, possui_deficiencia, cnh, passaporte, religiao, partido_politico, idioma, figurino, ) {
         this.musicalidade = musicalidade;
@@ -137,25 +138,117 @@ class Pessoas {
 
 class Musica {
     constructor (ritmo, velocidade, compasso, artista, nome, ano, tempo, idioma, tipo, genero, autor, compositor, musica, letra, instrumentos, volume, vocalista, baterista, guitarrista, baixista, sonoplasta) {
-
+        this.nome = nome;
+        this.artista = artista;
+        this.ritmo = ritmo;
+        this.genero = genero;
+        this.nome = nome;
+        this.velocidade = velocidade;
+        this.idioma = idioma;
+        this.ano = ano;
+        this.tempo = tempo;
+        this.tipo = tipo;
+        this.compasso = compasso;
+        this.autor = autor;
+        this.compositor = compositor;
+        this.musica = musica;
+        this.letra = letra;
+        this.instrumentos = instrumentos;
+        this.volume = volume;
+        this.vocalista = vocalista;
+        this.baterista = baterista;
+        this.guitarrista = guitarrista;
+        this.baixista = baixista;
+        this.sonoplasta = sonoplasta;
     }
 }
 
 class Ritmo {
     constructor (movimento, tempo, velocidade, som, compasso, coreografia, nome, quantidade_de_pessoas, musica, bpm, sincronia, harmonia, vestimentas, genero_musical, maquiagens, equipamentos, local_de_ensaio, melodia, composicao, compositor, instrumentos) {
-        // Ação ...
+        this.musica = musica;
+            this.movimento = movimento;
+            this.tempo = tempo;
+            this.velocidade = velocidade;
+            this.quantidade_de_pessoas = quantidade_de_pessoas;
+            this.som = som;
+            this.compasso = compasso;
+            this.coreografia = coreografia;
+            this.nome = nome;
+            this.bpm = bpm;
+            this.sincronia = sincronia;
+            this.harmonia = harmonia;
+            this.vestimentas = vestimentas;
+            this.genero_musical = genero_musical;
+            this.maquiagens = maquiagens;
+            this.equipamentos = equipamentos;
+            this.local_de_ensaio = local_de_ensaio;
+            this.melodia = melodia;
+            this.composicao = composicao;
+            this.compositor = compositor;
+            this.instrumentos = instrumentos;
     }
 }
 
 class LocalDoEnsaio {
     constructor (espaco, largura, altura, profundidade, energia, som, musica, microfone, palco, cadeira, capacidade_de_pessoas, equipamentos_de_luz, internet, cabo_de_energia, apresentacoes, stands, disponibilidade, seguranca, horario_de_funcionamento, acessibilidade, publico, artistas, janela, porta, camarim, comida, sala_de_descanso, escadas, ) {
+        this.espaco = espaco;
+        this.largura = largura;
+        this.altura = altura;
+        this.profundidade = profundidade;
+        this.energia = energia;
+        this.som = som;
+        this.musica = musica;
+        this.microfone = microfone;
+        this.palco = palco;
+        this.cadeira = cadeira;
+        this.capacidade_de_pessoas = capacidade_de_pessoas;
+        this.equipamentos_de_luz = equipamentos_de_luz;
+        this.internet = internet;
+        this.cabo_de_energia = cabo_de_energia;
+        this.apresentacoes = apresentacoes;
+        this.stands = stands;
+        this.disponibilidade = disponibilidade;
+        this.seguranca = seguranca;
+        this.horario_de_funcionamento = horario_de_funcionamento;
+        this.acessibilidade = acessibilidade;
+        this.publico = publico;
+        this.artistas = artistas;
+        this.janela = janela;
+        this.porta = porta;
+        this.camarim = camarim;
+        this.comida = comida;
+        this.sala_de_descanso = sala_de_descanso;
+        this.escadas = escadas;
 
     }
 }
 
 class Banheiro {
     constructor (genero, produtos, torneira, piz, vaso, piso, agua, energia, espelho, toalha_de_papel, suporte_de_sabonete, sabonete, suporte_de_bolsa, lixeira, descarga, mictorio, encanamento, odorizante, lampada, porta, interruptor, sensor_de_luz, janela, macaneta, batente) {
-
+        this.genero = genero;
+        this.produtos = produtos;
+        this.torneira = torneira;
+        this.vaso = vaso;
+        this.mictorio = mictorio;
+        this.espelho = espelho;
+        this.piso = piso;
+        this.agua = agua;
+        this.energia = energia;
+        this.toalha_de_papel = toalha_de_papel;
+        this.suporte_de_sabonete = suporte_de_sabonete;
+        this.sabonete = sabonete;
+        this.suporte_de_bolsa = suporte_de_bolsa;
+        this.lixeira = lixeira;
+        this.descarga = descarga;
+        this.encanamento = encanamento;
+        this.odorizante = odorizante;
+        this.lampada = lampada;
+        this.porta = porta;
+        this.interruptor = interruptor;
+        this.sensor_de_luz = sensor_de_luz;
+        this.janela = janela;
+        this.macaneta = macaneta;
+        this.batente = batente;
     }
 }
 
